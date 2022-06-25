eaf9ef8f266708a12b208e9abb3ab60b8b08487d
Sochuks <moseschukwumakajang@gmail.com>
2022-06-25 22:48:31 +0100

    Created django forms created templates(detail, update, delete) created blog urls(added to detail, update, delete) Created views(detail, update, delete )

--- I4G013983ANI/blog/urls.py
+++ I4G013983ANI/blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 # importing views from views.py
-from .views import PostCreateView, PostListView
+from .views import PostCreateView, PostListView, PostDetailView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostCreateView.as_view(), name='home'),
-    path('list/', PostListView.as_view(), name='list')
+    path('list/', PostListView.as_view(), name='list'),
+    # <pk> is identification for id field
+    path('detail/<pk>/', PostDetailView.as_view(), name='detail'),
+    path('update/<pk>', PostUpdateView.as_view(), name='update'),
+    path('delete/<pk>', PostDeleteView.as_view(), name='delete'),
 ]
--- I4G013983ANI/blog/views.py
+++ I4G013983ANI/blog/views.py
@@ -1,5 +1,7 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from .models import Post
 
 
@@ -16,6 +18,7 @@ class PostCreateView(CreateView):
     ]
 
     template_name = 'home.html'
+    success_url = "list.html"
 
 
 class PostListView(ListView):
@@ -23,4 +26,35 @@ class PostListView(ListView):
     # specify the model view for list
     model = Post
     template_name = 'list.html'
+
+
+class PostDetailView(DetailView):
+
+    # specify the model view for detail
+    model = Post
+    template_name = 'detail.html'
+
+
+class PostUpdateView(UpdateView):
+
+    # specify the model view for update
+    model = Post
+
+    # Specify fields to be used
+    fields = [
+        'title',
+        'body',
+    ]
+
+    template_name = 'update.html'
+    success_url = '/'
+
+
+class PostDeleteView(DeleteView):
+
+    # specify the model you want to use
+    model = Post
+
+    template_name = 'delete.html'
+    success_url = '/'
 
